a54b257fc1791cc82bafb294851157cf39ac10b9
chkrishnaa <163505418+chkrishnaa@users.noreply.github.com>
2025-01-09 13:19:19 +0530

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer>
-        <span>Created By <a href="https://www.linkedin.com/in/krishnakumar-chaurashiya-07021a286/">Krishnakumar</a> | <i className="fa-regular fa-copyright"></i> <span>2025<br/>All rights reserved.</span></span>
-    </footer>
-  )
-}
